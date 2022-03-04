1496d1835e33728659ee2e17171ac33ec77dbdce
Rabbi <jasrabbi50@gmail.com>
2022-03-04 22:07:18 +0600

    add problem description

--- find-the-symmetric-difference.js
+++ find-the-symmetric-difference.js
@@ -1,3 +1,18 @@
+//Problem Find Symmetric diffrence
+// Language: javascript
+//------------------------------------------------------------------------
+// The mathematical term symmetric difference (△ or ⊕) of two sets is the set of elements which are 
+// in either of the two sets but not in both. For example, for sets A = {1, 2, 3} and B = {2, 3, 4}, A △ B = {1, 4}.
+
+// Symmetric difference is a binary operation, which means it operates on only two elements. So to 
+// evaluate an expression involving symmetric differences among three elements (A △ B △ C), you 
+// must complete one operation at a time. Thus, for sets A and B above, and C = {2, 3}, A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
+
+// Create a function that takes two or more arrays and returns an array of their symmetric difference. 
+// The returned array must contain only unique values (no duplicates).
+
+
+
 const diff = (arg1, arg2) => [
   ...arg1.filter(e => !arg2.includes(e)),
   ...arg2.filter(e=> !arg1.includes(e))
